21cb72b07b05a4c9c442db429381701df86db696
Tuomo Virolainen <tuomo.virolainen@siili.com>
2018-12-29 17:21:17 +0200

    first commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject movies-2018 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [metasoarous/oz "1.4.1"]
+                 [kixi/stats "0.4.4"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/data.csv "0.1.4"]])
---
+++ src/movies_2018/core.clj
@@ -0,0 +1,105 @@
+(ns movies-2018.core
+  (:require [kixi.stats.core :as kixi]
+            [clojure.data.csv :as csv]
+            [clojure.string :as s]
+            [clojure.math.numeric-tower :as math]
+            [clojure.java.io :as io]
+            [oz.core :as oz]))
+
+(defn to-record [[position const created modified description title url title-type
+                  imdb-rating runtime year genres votes date directors your-rating _]]
+  {:created created
+   :watch-year (-> created (s/split #"-") first read-string)
+   :title title
+   :imdb-rating (read-string imdb-rating)
+   :runtime (if (= "" runtime) 0 (read-string runtime))
+   :year (read-string year)
+   :genres (-> genres (s/split #", ") set)
+   :votes (read-string votes)
+   :date date
+   :directors directors
+   :your-rating (when-not (= "" your-rating) (read-string your-rating))})
+
+(defn load-data []
+  (let [data (with-open [rdr (io/reader "./data/WATCHLIST.csv")]
+               (doall (csv/read-csv rdr)))]
+    (for [item (rest data)]
+      (try
+        (to-record item)
+        (catch Exception e
+          (println item))))))
+
+(defn seen-in? [year movie]
+  (-> movie :watch-year (= year)))
+
+(defn explain-time [mins]
+  (let [hours (/ mins 60.0)]
+    {:min mins
+     :hours hours
+     :days (/ hours 24.0)}))
+
+(defn length-counts-in [year data]
+  (transduce (comp (filter (partial seen-in? 2018)) (map :runtime)) + data))
+
+(defn genre-freqs [data]
+  (->> data
+       (map (comp vec :genres))
+       flatten
+       frequencies
+       (sort-by val)
+       reverse))
+
+(defn month-counts [data year]
+  (let [d (filter (partial seen-in? year) data)]
+    (->> d
+         (map (comp second #(s/split % #"-") :created))
+         frequencies
+         (map (fn [[k v]] {:month k :quantity v}))
+         (sort-by :month))))
+
+(defn rating-difference [{:keys [imdb-rating your-rating]}]
+  (when (every? number? [imdb-rating your-rating])
+    (math/abs (- your-rating imdb-rating))))
+
+(defn add-rating-difference [item]
+  (assoc item :rating-difference (rating-difference item)))
+
+(def month-histogram
+  {:data {:values (month-counts (load-data) 2018)}
+   :encoding {:x {:field "month" :type "ordinal"}
+              :y {:field "quantity" :type "quantitative"}}
+   :title "Films seen by month"
+   :mark "bar"})
+
+(defn top-rated [data year]
+  (let [d (filter (partial seen-in? year) data)]
+    (->> d
+         (filter #(not= nil (:your-rating %)))
+         (sort-by :your-rating)
+         reverse)))
+
+(defn genre-freq-map [data]
+  (for [[k v] (genre-freqs data)]
+    {:genre k :quantity v}))
+
+(def bars
+  {:data {:values (->> (load-data) (filter (partial seen-in? 2018)) genre-freq-map)}
+   :encoding {:x {:field "genre" :type "ordinal"}
+              :y {:field "quantity" :type "quantitative"}
+              :color {:field "genre" :type "nominal" :legend nil}}
+   :mark "bar"})
+
+(defn play-data [& names]
+  (for [n names
+        i (range 20)]
+    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
+
+(def line-plot
+  {:data {:values (play-data "monkey" "slipper" "broom")}
+   :encoding {:x {:field "time"}
+              :y {:field "quantity"}
+              :color {:field "item" :type "nominal"}}
+   :mark "line"})
+
+(defonce start
+  (oz/start-plot-server!))
